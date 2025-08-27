9d7609b8aa523521c4d473d6fc8bd88199bd5d5a
Osama Shaer <osamagivegh@gmail.com>
2025-08-27 09:39:58 +0300

    تعديلات جديدة

--- alshaerf-sh/models/User.js
+++ alshaerf-sh/models/User.js
@@ -1,10 +1,27 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
